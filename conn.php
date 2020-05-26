dd24fc7c5ca6bff9f39d141a2d2b76bcdaf43f4f
dev <amour-youcef@hotmail.com>
2020-05-26 15:53:13 +0200

    2605220

--- conn.php
+++
@@ -1,9 +0,0 @@
-
-<?php 
-try{
-    $bdd = new PDO("mysql:host=localhost;dbname=DB_CAT_DWM;charset=utf8" , "root" ,"");
-}catch (PDOException $e){
-    echo " errer connexion base de donne";
-};
-?>
-
